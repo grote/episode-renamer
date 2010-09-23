cf041f0c1910af1c10053ef0f855b2c38a483adc
Torsten Grote <Torsten.Grote@fsfe.org>
2010-09-23 14:05:48 +0200

    removed BeautifulSoup dependency, so packaging for modern distros is possible

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(
         'Programming Language :: Python',
         'Topic :: Multimedia :: Video',
     ],
-    install_requires=["BeautifulSoup==3.0.8.1",
+#   install_requires=["BeautifulSoup==3.0.8.1",
+    install_requires=[
                       "simplejson>=2.0.9"],
     entry_points = {
         'console_scripts':[
